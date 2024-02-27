e1b1932fcdcd040194903b617ef9167f58379e2a
Faytoon4u <144126823+Faytoon4u@users.noreply.github.com>
2024-02-27 09:49:52 +0100

    up to date
    now my character can walk again, i removed the "load data"

--- js/main.js
+++ js/main.js
@@ -1,129 +1,125 @@
-document.getElementById("mainTitle").innerText =
-  "Point and Click adventure game";
+document.getElementById("mainTitle").innerText = "Point and Click adventure game";
+// Game State
+let gameState = {
+    "inventory": [],
+    "coinPickedUp": false
+}
+
+// // load data from save file
+// fetch('data/save.json').then((response) => {
+//     if (response.status == 404) {
+//         alert('file not found!');
+//     } else {
+//         return response.json();
+//     }
+// }).then((resJson) => {
+//     gameState = resJson;
+//     runGame();
+// }).catch((error) => {
+//     console.error(error)
+// })
+
+function runGame() {
+    //Game window reference
+    const gameWindow = document.getElementById("gameWindow");
+    const inventoryList = document.getElementById("inventoryList");
+
+    //Main Character
+    const mainCharacter = document.getElementById("hero");
+    const offsetCharacter = 16;
+    const tree1 = document.getElementById("squareTree");
 
 
-//Game window reference
-const gameWindow = document.getElementById("gameWindow");
+    gameWindow.onclick = function (e) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
 
-//Game State
-gameState = {
-  inventory: [],
-  coinPickedUp: false,
-};
+        //TODO: calc offset based on character size
+        //TODO: making dialog functionality
 
-//Inventory
-let inventory = [];
-console.log(inventory);
-const inventoryList = document.getElementById("inventoryList");
-//Main Character
-const mainCharacter = document.getElementById("hero");
-const offsetCharacter = 16;
-const tree1 = document.getElementById("squareTree");
+        if (e.target.id !== "heroImage") {
+            mainCharacter.style.left = x - offsetCharacter + "px";
+            mainCharacter.style.top = y - offsetCharacter + "px";
+        }
 
-gameWindow.onclick = function (e) {
-  var rect = gameWindow.getBoundingClientRect();
-  var x = e.clientX - rect.left;
-  var y = e.clientY - rect.top;
-
-  if (e.target.id !== "heroImage") {
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
-  }
-
-  switch (e.target.id) {
-    case "squareTree":
-      tree1.style.opacity = 0.5;
-    case "key":
-      console.log("pick up key");
-      changeInventory("key", "add");
-      document.getElementById("key").remove();
-      break;
-    case "well":
-      if (gameState.coinPickedUp == false) {
-        changeInventory("coin", "add");
-        gameState.coinPickedUp = true;
-      } else {
-        console.log("There are no more coins in this well!!");
-      }
-      break;
-    case "doorWizardHut":
-      if (checkItem("key")) {
-        console.log("I opend the door. Yeah!");
-      } else if (checkItem("coin")) {
-        changeInventory("coin", "remove");
-        console.log(
-          "Oh no i lost het coin and I didn't open the door... I feel kinda stupid!"
-        );
-      } else {
-        console.log("Fuck the door is locked. I don't have the key!");
-      }
-      break;
-    case "statue":
-      {
-        console.log(
-          "heey you.... wanna know where the key is?? It is by the graves. Good luck looking."
-        );
-      }
-      break;
-    default:
-      break;
-  }
-  /**
-   * Add or remove item from inventory.
-   * @param {string} itemName
-   * @param {string} action
-   */
-  function changeInventory(itemName, action) {
-    if (itemName == null || action == null) {
-      console.error("wrong parameters given to changeInventory()");
-      return;
+        switch (e.target.id) {
+            case "key":
+                console.log("pick up key")
+                document.getElementById("key").remove();
+                changeInventory('key', "add");
+                break;
+            case "well":
+                if (gameState.coinPickedUp == false) {
+                    changeInventory("coin", "add");
+                    gameState.coinPickedUp = true;
+                } else {
+                    console.log("There are no more coins in this well!");
+                }
+                break;
+            case "doorWizardHut":
+                if (checkItem("key")) {
+                    console.log("I opened the door. Yeah!");
+                } else if (checkItem("coin")) {
+                    changeInventory("coin", "remove");
+                    console.log("Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..");
+                } else {
+                    console.log("Fuck this door is locked and I don't have a key. boohoo :(");
+                }
+                break;
+            case "statue":
+                console.log("hey you.. wanna know where the key is? It's by the graves.");
+                break;
+            default:
+                break;
+        }
     }
 
-    switch (action) {
-      case "add":
-        gameState.inventory.push(itemName);
-        break;
-      case "remove":
-        gameState.inventory = gameState.inventory.filter(function (newInventory) {
-          return newInventory !== itemName;
-        });
-        document.getElementById("inv-" + itemName).remove();
-        break;
+    /**
+     * Add or remove item in inventory
+     * @param {string} itemName 
+     * @param {string} action 
+     */
+    function changeInventory(itemName, action) {
+        if (itemName == null || action == null) {
+            console.error("Wrong parameters given to changeInventory()");
+            return;
+        }
+
+        switch (action) {
+            case 'add':
+                gameState.inventory.push(itemName);
+                break;
+            case 'remove':
+                gameState.inventory = gameState.inventory.filter(function (newInventory) {
+                    return newInventory !== itemName;
+                });
+                document.getElementById("inv-" + itemName).remove();
+                break;
+
+        }
+        updateInventory(gameState.inventory, inventoryList);
     }
-    updateInventory(gameState.inventory, inventoryList);
-  }
 
-  /**
-   * checks if the value exists within the array
-   * if not then it adds value to the array and use showItem function
-   * @param {string} itemName
-   * @param {string} itemId
-   */
-
-  function getItem(itemName, itemId) {
-    if (!checkItem(itemName)) {
-      inventory.push(itemName);
-      showItem(itemName, itemId);
+    /**
+     * This returns string value if it exist within the array
+     * @param {string} itemName 
+     * @returns 
+     */
+    function checkItem(itemName) {
+        return gameState.inventory.includes(itemName);
     }
-    console.log(inventory);
-  }
 
-  /**
-   * removes item from array and the inventory
-   * @param {string} itemName
-   * @returns
-   */
-  function checkItem(itemName) {
-    return gameState.inventory.includes(itemName);
-  }
+    function updateInventory(inventory, inventoryList) {
+        inventoryList.innerHTML = '';
+        inventory.forEach(function (item) {
+            const inventoryItem = document.createElement("li");
+            inventoryItem.id = 'inv-' + item;
+            inventoryItem.innerText = item;
+            inventoryList.appendChild(inventoryItem);
+        })
+    }
 
-  function updateInventory(inventory, inventoryList) {
-    inventoryList.innerHTML = "";
-    inventory.forEach(function (item) {
-      const inventoryItem = document.createElement("li");
-      inventoryItem.id = "inv-" + item;
-      inventoryItem.innerText = item;
-      inventoryList.appendChild(inventoryItem);
-    });
-  }
-};
+
+}
+runGame();
